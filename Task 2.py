9ba7b4152a140d19e385000ba436e773a0cfad06
Rachit gour <rachitgour777@gmail.com>
2026-01-05 23:02:28 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,9 @@
+
+def sum_num(n):
+    total = 0
+    for number in range(1, 51):
+        total += number
+    return total
+
+result = sum_num(50)
+print("The sum of numbers from 1 to 50 is:", result)
